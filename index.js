99b6f6821449e5d85cb6d4220495f1ce2b75c803
mattfrankly <31962021+mattfrankly@users.noreply.github.com>
2017-10-20 11:05:39 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ class ModuleDemo extends Component {
   render(){
 	  
     return (
-      <div className='demo_title'>
+      <div className='demo_text'>
 	<h2>{this.props.text}</h2>
       </div>
     );
