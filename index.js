b16d9ac441c1b1872cbeabdf98df246d0601f3c9
mattfrankly <31962021+mattfrankly@users.noreply.github.com>
2017-10-20 10:58:44 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+import React, { Component, PropTypes } from 'react';
+
+class ModuleDemo extends Component {
+
+  render(){
+	  
+    return (
+      <div>
+	<h2>{this.props.text}</h2>
+      </div>
+    );
+	  
+  }
+}
+
+export default ModuleDemo;
