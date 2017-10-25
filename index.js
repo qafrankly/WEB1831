1e02333ae58559f05dfe72692a280bfb11605d18
mattfrankly <31962021+mattfrankly@users.noreply.github.com>
2017-10-25 14:46:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,17 +8,6 @@ class HomePageTakeover extends Component {
   }
 
 
-  componentWillMount() { /* OPTIONAL, fires before initial mount into DOM. Setting state here or editing props here will not cause a re-render*/ }
-  componentDidMount() {/* OPTIONAL, fires after initial rendering ( based on initial default state ), DOM can be accessed here if needed. */ }
-
-  componentWillReceiveProps(nextProps) {/* OPTIONAL, fired when parent component has re-rendered or made other changes to the props, allows us to conditionaal change our state only when needed*/ }
-  shouldComponentUpdate(nextProps, nextState) {/* OPTIONAL, allows us to intelligently decide if we really need to re-render (BOOL)*/
-    return true
-  }
-  componentWillUpdate(nextProps, nextState) {/*OPTIONAL (and cannot call setState in here) last opportunity to massage data before render */ }
-  componentDidUpdate(prevProps, prevState) { /* OPTIONAL Here we have access to the DOM again, */ }
-
-  componentWillUnmount() {/* GREAT place to kill any network requests, or any Timeout or Interval functions trying up resources. Last stop before we are destroyed.  */ }
 
 
   render() { //REQUIRED
