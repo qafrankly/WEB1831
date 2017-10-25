e1682a93fbb84e0cae079193436b286ab87eef89
mattfrankly <31962021+mattfrankly@users.noreply.github.com>
2017-10-25 15:02:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 
-class HomePageTakeover extends Component {
-  render() { //REQUIRED
-    return (<div className='gnm-home-page-takeover'>TAKEOVER</div>)
+class ModuleDemo extends Component {
+
+  render(){
+	  
+    return (
+      <div className='FranklyModulesDemo'>
+	<h2 className='demo_text'>{this.props.text}</h2>
+	<p>This text is not editable!</p>
+      </div>
+    );
+	  
   }
 }
 
-export default HomePageTakeover;
+export default ModuleDemo;
