5eb79f342422bb778f3af29004462c0a0c3b65e1
mattfrankly <31962021+mattfrankly@users.noreply.github.com>
2017-10-25 15:01:08 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class HomePageTakeover extends Component {
   render() { //REQUIRED
-    return (<div className="gnm-home-page-takeover">TAKEOVER</div>)
+    return (<div className='gnm-home-page-takeover'>TAKEOVER</div>)
   }
 }
 
