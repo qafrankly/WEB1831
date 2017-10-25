4c709667485c9d033e6fb00215bcfb968367882f
mattfrankly <31962021+mattfrankly@users.noreply.github.com>
2017-10-25 14:58:39 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ class ModuleDemo extends Component {
   render(){
 	  
     return (
-      <div className="FranklyModulesDemo">
+      <div className="gnm-home-page-takeover">
 	<h2 className='demo_text'>{this.props.text}</h2>
 	<p>This text is not editable!</p>
       </div>
