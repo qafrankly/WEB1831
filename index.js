a71e87b969058c7804c6c41561dbf933566abd55
mattfrankly <31962021+mattfrankly@users.noreply.github.com>
2017-11-09 12:55:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ import React, { Component } from 'react';
 
 class HomePageTakeover extends Component {
 
-  constructor(props){ //gives us acces to props, fires long before page load
-    super(props) //assigns props to this.props
-    this.state = {} /* great place to assign default state */
-  }
+
  
 
   render(){ //REQUIRED
     return ()
   }
+  
 }
 
 export default HomePageTakeover;
