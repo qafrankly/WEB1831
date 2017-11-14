b0119d4e9cb341583d727198924d18a919a95a57
mattfrankly <31962021+mattfrankly@users.noreply.github.com>
2017-11-14 15:40:58 -0500

    Create test.js

---
+++ test.js
@@ -0,0 +1,16 @@
+import React, { Component, PropTypes } from 'react';
+
+class Test extends Component {
+
+  render(){
+	  
+    return (
+      <div className='FranklyModulesDemo'>
+	      <p>Test</p>
+      </div>
+    );
+	  
+  }
+}
+
+export default Test;
