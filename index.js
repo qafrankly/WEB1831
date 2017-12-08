365a69c83bfd52f03a6be5b5ceb731fe073b55c4
mattfrankly <31962021+mattfrankly@users.noreply.github.com>
2017-12-08 18:35:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ class ModuleDemo extends Component {
       <div className='FranklyModulesDemo'>
 	<h2 className='demo_text'>{this.props.text}</h2>
 	<p>This text is not editable!</p>
-	<Image width={280} src={'http://kwtv.images.worldnow.com/images/14434055_G.jpg'} alt={'alt'} />
+	<Image width={280} src={'http://franklydemo.images.worldnow.com/images/15208935_G.jpeg'} alt={'alt'} />
       </div>
     );
 	  
