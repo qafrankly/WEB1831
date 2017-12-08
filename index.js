f1a9f6de039e31d9d754a2a7d0a4699515efedb2
mattfrankly <31962021+mattfrankly@users.noreply.github.com>
2017-12-08 15:33:33 -0500

    testing image

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import Image from 'components/Image';
 
 class ModuleDemo extends Component {
 
@@ -8,6 +9,7 @@ class ModuleDemo extends Component {
       <div className='FranklyModulesDemo'>
 	<h2 className='demo_text'>{this.props.text}</h2>
 	<p>This text is not editable!</p>
+	<Image width={280} src={'http://kwtv.images.worldnow.com/images/14434055_G.jpg'} alt={'alt'} />
       </div>
     );
 	  
