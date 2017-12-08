ef53171424e15ce185b47e29677c8bb17e1eeb69
mattfrankly <31962021+mattfrankly@users.noreply.github.com>
2017-12-08 18:01:34 -0500

    Adding image

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-// import Image from 'components/Image';
-// 	<Image width={280} src={'http://kwtv.images.worldnow.com/images/14434055_G.jpg'} alt={'alt'} />
+import Image from 'components/Image';
 
 class ModuleDemo extends Component {
 
@@ -10,6 +9,7 @@ class ModuleDemo extends Component {
       <div className='FranklyModulesDemo'>
 	<h2 className='demo_text'>{this.props.text}</h2>
 	<p>This text is not editable!</p>
+	<Image width={280} src={'http://kwtv.images.worldnow.com/images/14434055_G.jpg'} alt={'alt'} />
       </div>
     );
 	  
